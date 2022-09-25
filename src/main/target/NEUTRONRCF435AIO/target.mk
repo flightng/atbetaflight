8f27b4d0c11bb38dfda5de268ef08bcf328792c7
shanggl <shangl@wo.cn>
2022-09-25 21:52:07 +0800

    add  NeutronF435Aio LQFP64 target

---
+++ src/main/target/NEUTRONRCF435AIO/target.mk
@@ -0,0 +1,28 @@
+AT32F43xxMT7_TARGETS += $(TARGET)
+
+CUSTOM_DEFAULTS_EXTENDED = yes
+FEATURES       +=  VCP ONBOARDFLASH
+
+TARGET_SRC = \
+			drivers/accgyro/accgyro_mpu6500.c \
+            drivers/accgyro/accgyro_spi_mpu6000.c \
+            drivers/accgyro/accgyro_spi_mpu6500.c\
+            drivers/accgyro/accgyro_spi_icm426xx.c\
+            drivers/barometer/barometer_bmp280.c \
+            drivers/barometer/barometer_dps310.c\
+            drivers/compass/compass_hmc5883l.c\
+            drivers/compass/compass_qmc5883l.c\
+            $(ROOT)/lib/main/BoschSensortec/BMI270-Sensor-API/bmi270_maximum_fifo.c \
+            drivers/accgyro/accgyro_spi_bmi270.c\
+            drivers/accgyro/accgyro_spi_lsm6dso_init.c \
+            drivers/accgyro/accgyro_spi_lsm6dso.c \
+            drivers/max7456.c \
+            drivers/max7456.c \
+            drivers/vtx_rtc6705.c \
+            drivers/vtx_rtc6705_soft_spi.c \
+            drivers/rx/expresslrs_driver_atbsp.c \
+            drivers/rx/rx_sx127x.c \
+            drivers/rx/rx_sx1280.c \
+            rx/expresslrs_telemetry.c \
+            rx/expresslrs_common.c \
+            rx/expresslrs.c
