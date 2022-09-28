eba6a0f3ec5e8b5c5d141de6a54c6d829a31cfab
shanggl <shangl@wo.cn>
2022-09-28 21:31:46 +0800

    # WARNING: head commit changed in the meantime
    Merge branch 'AT32F43x-ATBSP' of https://github.com/shanggl/ATBetaflight
    into AT32F43x-ATBSP
    
    # Conflicts:
    #	.github/FUNDING.yml

--- src/main/target/common_pre.h
+++ src/main/target/common_pre.h
@@ -82,7 +82,7 @@
 #define USE_DYN_NOTCH_FILTER
 #define USE_ADC_INTERNAL
 //#define USE_USB_CDC_HID
-//#define USE_USB_MSC
+#define USE_USB_MSC
 #define USE_PERSISTENT_MSC_RTC
 #define USE_MCO
 #define USE_DMA_SPEC
