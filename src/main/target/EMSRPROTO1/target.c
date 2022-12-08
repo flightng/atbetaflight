d60996a7e0c61a84b5bf184edd00eb4f3a4924c3
emsr <shanggl@wo.cn>
2022-12-08 13:45:13 +0000

    replace servo 1234 with motor 5678

--- src/main/target/EMSRPROTO1/target.c
+++ src/main/target/EMSRPROTO1/target.c
@@ -23,21 +23,21 @@
 #include "platform.h"
 #include "drivers/io.h"
 
- #include "drivers/dma.h"
- #include "drivers/timer.h"
- #include "drivers/timer_def.h"
+#include "drivers/dma.h"
+#include "drivers/timer.h"
+#include "drivers/timer_def.h"
 
- const timerHardware_t timerHardware[USABLE_TIMER_CHANNEL_COUNT] = {
- 	DEF_TIM(TMR2, CH3, PB10, TIM_USE_ANY |TIM_USE_LED, 0,13,0), // PWM1 - OUT1 MCO1 DMA1 CH2
-	DEF_TIM(TMR2, CH4, PB11,  TIM_USE_ANY |TIM_USE_BEEPER, 0,12,0), // PWM2 - OUT1 DMA1 CH6
-	DEF_TIM(TMR3, CH1, PC6,  TIM_USE_ANY |TIM_USE_SERVO, 0,11,10), // PWM3 - OUT3  DMA1 CH7
-	DEF_TIM(TMR3, CH2, PC7,  TIM_USE_ANY |TIM_USE_SERVO, 0,10,10), // PWM4 - OUT4  DMA2 CH1
-	DEF_TIM(TMR3, CH3, PC8,  TIM_USE_ANY |TIM_USE_SERVO, 0,9,10), // PWM5 - OUT5  DMA2 CH2
-	DEF_TIM(TMR3, CH4, PC9,  TIM_USE_ANY |TIM_USE_SERVO, 0,8,10), // PWM6 - OUT6  DMA2 CH3
+const timerHardware_t timerHardware[USABLE_TIMER_CHANNEL_COUNT] = {
+    DEF_TIM(TMR2, CH3, PB10, TIM_USE_ANY | TIM_USE_LED, 0, 13, 0),    // PWM1 - OUT1 MCO1 DMA1 CH2
+    DEF_TIM(TMR2, CH4, PB11, TIM_USE_ANY | TIM_USE_BEEPER, 0, 12, 0), // PWM2 - OUT1 DMA1 CH6
 
- 	DEF_TIM(TMR4, CH1, PB6,  TIM_USE_MOTOR, 0,0,1),  // motor1 DMA2 CH7
- 	DEF_TIM(TMR4, CH2, PB7,  TIM_USE_MOTOR, 0,2,1),  // motor2 DMA2 CH6
- 	DEF_TIM(TMR4, CH3, PB8,  TIM_USE_MOTOR,  0,1,0), // motor3 DMA2 CH5
- 	DEF_TIM(TMR4, CH4, PB9,  TIM_USE_MOTOR,  0,3,0), // motor4 DMA2 CH4
+    DEF_TIM(TMR4, CH1, PB6, TIM_USE_MOTOR, 0, 0, 1),                 // motor1 DMA1 CH1
+    DEF_TIM(TMR4, CH2, PB7, TIM_USE_MOTOR, 0, 2, 1),                 // motor2 DMA1 CH3
+    DEF_TIM(TMR4, CH3, PB8, TIM_USE_MOTOR, 0, 1, 0),                 // motor3 DMA1 CH2
+    DEF_TIM(TMR4, CH4, PB9, TIM_USE_MOTOR, 0, 3, 0),                 // motor4 DMA1 CH4
+    DEF_TIM(TMR3, CH1, PC6, TIM_USE_ANY | TIM_USE_MOTOR, 0, 11, 10), // motor 5
+    DEF_TIM(TMR3, CH2, PC7, TIM_USE_ANY | TIM_USE_MOTOR, 0, 10, 10), // motor 6
+    DEF_TIM(TMR3, CH3, PC8, TIM_USE_ANY | TIM_USE_MOTOR, 0, 9, 10),  // motor 7
+    DEF_TIM(TMR3, CH4, PC9, TIM_USE_ANY | TIM_USE_MOTOR, 0, 8, 10),  // motor 8
 
- };
+};
--- src/main/target/NEUTRONRCF435AIO/target.c
+++ src/main/target/NEUTRONRCF435AIO/target.c
@@ -23,23 +23,22 @@
 #include "platform.h"
 #include "drivers/io.h"
 
- #include "drivers/dma.h"
- #include "drivers/timer.h"
- #include "drivers/timer_def.h"
+#include "drivers/dma.h"
+#include "drivers/timer.h"
+#include "drivers/timer_def.h"
 
- const timerHardware_t timerHardware[USABLE_TIMER_CHANNEL_COUNT] = {
- 	DEF_TIM(TMR5, CH1, PA0, TIM_USE_ANY |TIM_USE_CAMERA_CONTROL, 0,13,0), // PWM1 - OUT1 MCO1 DMA1 CH2
- 	DEF_TIM(TMR5, CH2, PA1, TIM_USE_ANY |TIM_USE_LED, 0,7,0), // PWM1 - OUT1 MCO1 DMA1 CH2
-	DEF_TIM(TMR2, CH4, PA3,  TIM_USE_ANY |TIM_USE_PPM, 0,6,0), // PWM2 - OUT1 DMA1 CH6
+const timerHardware_t timerHardware[USABLE_TIMER_CHANNEL_COUNT] = {
+    DEF_TIM(TMR5, CH1, PA0, TIM_USE_ANY | TIM_USE_CAMERA_CONTROL, 0, 13, 0), // PWM1 - OUT1 MCO1 DMA1 CH2
+    DEF_TIM(TMR5, CH2, PA1, TIM_USE_ANY | TIM_USE_LED, 0, 7, 0),             // PWM1 - OUT1 MCO1 DMA1 CH2
+    DEF_TIM(TMR2, CH4, PA3, TIM_USE_ANY | TIM_USE_PPM, 0, 6, 0),             // PWM2 - OUT1 DMA1 CH6
 
-	DEF_TIM(TMR4, CH1, PB6,  TIM_USE_ANY |TIM_USE_SERVO, 0,11,10), // PWM5 - DMA2 CH5
-	DEF_TIM(TMR4, CH2, PB7,  TIM_USE_ANY |TIM_USE_SERVO, 0,10,10), // PWM6 - DMA2 CH4
-	DEF_TIM(TMR3, CH4, PB1,  TIM_USE_ANY |TIM_USE_SERVO, 0,8,10), // PWM7 - DMA2 CH2
-	DEF_TIM(TMR3, CH3, PB0,  TIM_USE_ANY |TIM_USE_SERVO, 0,9,10), // PWM8 - DMA2 CH3
+    DEF_TIM(TMR8, CH4, PC9, TIM_USE_MOTOR, 0, 0, 1),                 // motor1 DMA1 CH1
+    DEF_TIM(TMR8, CH3, PC8, TIM_USE_MOTOR, 0, 2, 1),                 // motor2 DMA1 CH3
+    DEF_TIM(TMR3, CH2, PC7, TIM_USE_MOTOR, 0, 1, 0),                 // motor3 DMA1 CH1
+    DEF_TIM(TMR3, CH1, PC6, TIM_USE_MOTOR, 0, 3, 0),                 // motor4 DMA1 CH4
+    DEF_TIM(TMR4, CH1, PB6, TIM_USE_ANY | TIM_USE_MOTOR, 0, 11, 10), // motor5 DMA2 CH5
+    DEF_TIM(TMR4, CH2, PB7, TIM_USE_ANY | TIM_USE_MOTOR, 0, 10, 10), // motor6 DMA2 CH4
+    DEF_TIM(TMR3, CH4, PB1, TIM_USE_ANY | TIM_USE_MOTOR, 0, 8, 10),  // motor7 DMA2 CH2
+    DEF_TIM(TMR3, CH3, PB0, TIM_USE_ANY | TIM_USE_MOTOR, 0, 9, 10),  // motor8 DMA2 CH3
 
- 	DEF_TIM(TMR8, CH4, PC9,  TIM_USE_MOTOR, 0,0,1),  // motor1 DMA1 CH1
- 	DEF_TIM(TMR8, CH3, PC8,  TIM_USE_MOTOR, 0,2,1),  // motor2 DMA1 CH3
- 	DEF_TIM(TMR3, CH2, PC7,  TIM_USE_MOTOR, 0,1,0), // motor3 DMA1 CH1
- 	DEF_TIM(TMR3, CH1, PC6,  TIM_USE_MOTOR, 0,3,0), // motor4 DMA1 CH4
-
- };
+};
