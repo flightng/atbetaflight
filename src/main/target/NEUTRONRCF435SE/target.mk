9fed0c3c4bf80aacc485909f9c492332b19d35ba
Hugo Chiang <1483569698@qq.com>
2023-02-22 22:09:28 +0800

    [IMU Driver]: Add CRT to BMI270 (#31)
    * add crt for bmi270
    
    * remove 6500 to fix cli lock issue

--- src/main/target/NEUTRONRCF435SE/target.mk
+++ src/main/target/NEUTRONRCF435SE/target.mk
@@ -5,9 +5,7 @@ CUSTOM_DEFAULTS_EXTENDED = yes
 FEATURES       +=  VCP ONBOARDFLASH
 
 TARGET_SRC = \
-			drivers/accgyro/accgyro_mpu6500.c \
             drivers/accgyro/accgyro_spi_mpu6000.c \
-            drivers/accgyro/accgyro_spi_mpu6500.c\
             drivers/accgyro/accgyro_spi_icm426xx.c\
             drivers/barometer/barometer_bmp280.c \
             drivers/barometer/barometer_dps310.c\
