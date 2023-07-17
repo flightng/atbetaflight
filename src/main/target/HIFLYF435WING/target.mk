a133755d6528352263311ccceebd77a753baf2de
Hifly-rc <54883661@qq.com>
2023-07-17 19:59:47 +0800

    update imu define

--- src/main/target/HIFLYF435WING/target.mk
+++ src/main/target/HIFLYF435WING/target.mk
@@ -5,8 +5,6 @@ CUSTOM_DEFAULTS_EXTENDED = yes
 FEATURES       +=  VCP ONBOARDFLASH
 
 TARGET_SRC = \
-        drivers/accgyro/accgyro_mpu6500.c \
-        drivers/accgyro/accgyro_spi_mpu6500.c \
         drivers/accgyro/accgyro_spi_icm426xx.c\
         drivers/barometer/barometer_bmp280.c \
         drivers/barometer/barometer_dps310.c\
